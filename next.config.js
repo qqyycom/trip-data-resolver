285635b498282aae9f8b0aa2c1415f678cb37aff
Kyrie Qu <kyrie.qu@foxxusa.com>
2025-11-21 16:08:46 +0800

    chore: fix build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const path = require("path")
+const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  outputFileTracingRoot: path.join(__dirname, ".."),
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
